f85b93d634e163c13a3b860102be2ef2ded829e3
Littlechu <hoi.fai@gmail.com>
2025-08-16 16:01:03 -0700

    Use the latest version of ComfyUI and install comfyui-manager by default

--- services/download/download.sh
+++ services/download/download.sh
@@ -6,20 +6,11 @@ set -Eeuo pipefail
 mkdir -vp /data/.cache \
   /data/embeddings \
   /data/config/ \
-  /data/models/ \
-  /data/models/Stable-diffusion \
-  /data/models/GFPGAN \
-  /data/models/RealESRGAN \
-  /data/models/LDSR \
-  /data/models/VAE
+  /data/models/
 
 echo "Downloading, this might take a while..."
 
 aria2c -x 10 --disable-ipv6 --input-file /docker/links.txt --dir /data/models --continue
-
-echo "Checking SHAs..."
-
-parallel --will-cite -a /docker/checksums.sha256 "echo -n {} | sha256sum -c"
 
 cat <<EOF
 By using this software, you agree to the following licenses:
